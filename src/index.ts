4e292d594ae1ca13b8d2e1f60ed73b53d7d21b2d
ibkay998 <ibukun46@gmail.com>
2022-11-05 20:40:44 +0100

    new

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,7 @@ const app = express();
 const port = process.env.PORT || 3333;
 
 app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
+
 
 app.get("/", async (req, res) => {
   res.json({ message: "Please visit /countries to view all the countries" });
@@ -18,7 +17,7 @@ app.post('/posts', (req,res) => {
   res.status(200).json({
       "slackUsername":"JerryG",
       "result": result,
-      
+      "operation_type":req.body.operation_type
       // "operation_type": req.body.operation_type
   })
 
